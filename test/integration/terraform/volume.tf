81f323f385311f6930fb2fa8f42ade381a6414e5
kris <kristen.f.jacobs@oracle.com>
2017-10-23 14:50:22 +0100

    Fix wercker test piplines

--- test/integration/terraform/volume.tf
+++ test/integration/terraform/volume.tf
@@ -2,7 +2,7 @@ resource "oci_core_volume" "test_volume" {
   availability_domain = "${var.availability_domain}"
   compartment_id = "${var.compartment_ocid}"
   display_name = "${var.flexvolume_test_id}"
-  size_in_gbs = "50"
+  size_in_mbs = "51200"  # 50GB
 }
 
 output "volume_ocid" {
--- test/system/terraform/volume.tf
+++ test/system/terraform/volume.tf
@@ -40,7 +40,7 @@ resource "oci_core_volume" "test_volume" {
   availability_domain = "${var.availability_domain}"
   compartment_id = "${var.compartment_ocid}"
   display_name = "flexvolumesystemtest${var.test_id}"
-  size_in_mbs = "51200"  # 50GB
+  size_in_gbs = "50"
 }
 
 output "volume_ocid" {
