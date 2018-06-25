98ada0ae959d4d8876669a905fc155b9df25dfe1
Harvey Lowndes <harvey.lowndes@oracle.com>
2018-06-25 09:53:05 +0100

    Support using symlinks in storage class to find correct driver

--- cmd/oci/main.go
+++ cmd/oci/main.go
@@ -18,6 +18,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/oracle/oci-flexvolume-driver/pkg/flexvolume"
 	"github.com/oracle/oci-flexvolume-driver/pkg/oci/driver"
@@ -27,6 +29,13 @@ import (
 var version string
 var build string
 
+// DefaultDriver is the default flexvolume driver symlink extension.
+const DefaultDriver string = "oci-bvs"
+
+var (
+	drivers = make(map[string]flexvolume.Driver)
+)
+
 // GetLogPath returns the default path to the driver log file.
 func GetLogPath() string {
 	path := os.Getenv("OCI_FLEXD_DRIVER_LOG_DIR")
@@ -34,6 +43,10 @@ func GetLogPath() string {
 		path = driver.GetDriverDirectory()
 	}
 	return path + "/oci_flexvolume_driver.log"
+}
+
+func registerDrivers() {
+	registerDriver("oci-bvs", &driver.OCIFlexvolumeDriver{})
 }
 
 func main() {
@@ -50,5 +63,53 @@ func main() {
 	log.SetOutput(f)
 
 	log.Printf("OCI FlexVolume Driver version: %s (%s)", version, build)
-	flexvolume.ExecDriver(&driver.OCIFlexvolumeDriver{}, os.Args)
+
+	registerDrivers()
+
+	driver, err := getDriverFromArgs()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	flexvolume.ExitWithResult(flexvolume.ExecDriver(driver, os.Args))
+}
+
+func getDriverFromArgs() (flexvolume.Driver, error) {
+	driver, err := getDriver(DefaultDriver) //Block volume is default
+	if err != nil {
+		return nil, err
+	}
+
+	if len(os.Args) == 0 {
+		log.Printf("No arguments found, using default driver %s", DefaultDriver)
+		return driver, nil
+	}
+
+	dir := path.Base(os.Args[0])
+	dir = strings.TrimPrefix(dir, "oracle~")
+
+	if dir != "oci" && dir != DefaultDriver {
+		driver, err = getDriver(dir)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	log.Printf("Using %s driver", dir)
+
+	return driver, nil
+}
+
+func registerDriver(name string, driver flexvolume.Driver) {
+	if drivers[name] == nil {
+		drivers[name] = driver
+	}
+}
+
+func getDriver(name string) (flexvolume.Driver, error) {
+	driver, ok := drivers[name]
+	if !ok {
+		return nil, fmt.Errorf("could not find a registered driver for %s", name)
+	}
+	return driver, nil
 }
